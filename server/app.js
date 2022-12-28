ff316bbf010cde371e7c090551a2a66171e1f69f
Deepankur Lohiya <ankurlohiya3@gmail.com>
2022-12-28 13:28:43 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.use("/api/report", reportRoutes);
 const path=require("path");
 __dirname=path.resolve();
 
+app.use(express.static(path.join(__dirname,'./client/build')))
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"/client/build")));
   app.get("*",(req,res)=>{
